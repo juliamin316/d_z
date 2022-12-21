0e6234070f78d80b95257217d5cf55e8282dfa9d
juliamin316 <121152468+juliamin316@users.noreply.github.com>
2022-12-21 22:44:31 +0600

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,41 +1,36 @@
-Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-def read_data(file_name):
-...     file = open(file_name)
-...     content = file.read()
-...     return content
-... 
-... 
-... def write_data(file_name, data):
-...     f = open(file_name, "w")
-...     f.write(data)
-...     f.close()
-... 
-... 
-... def prepare_lines(data):
-...     splitted = data.split('\n')
-...     titles, line_1, line_2, line_3 = splitted[0], splitted[1], splitted[2], splitted[3]
-...     labels = titles.split(',')
-...     line_1_formatted = line_1.split(',')
-...     line_2_formatted = line_2.split(',')
-...     line_3_formatted = line_3.split(',')
-...     # print(line_1_formatted)
-...     one = dict(zip(labels, line_1_formatted))
-...     two = dict(zip(labels, line_2_formatted))
-...     three = dict(zip(labels, line_3_formatted))
-...     # l = [one, two, three]
-...     print(one, two, three, sep="\n")
-...     # print(l)
-... 
-...     return labels, line_1_formatted, line_2_formatted, line_3_formatted,
-... 
-... 
-... def format_text(labels, line_1_formatted, line_2_formatted, line_3_formatted):
-...     d = dict(zip(labels, line_1_formatted))
-...     print(d)
-... 
-...     
-... 
-... 
-... 
-... content = read_data("input.py")
+def read_data(file_name): 
+    file = open(file_name) 
+    content = file.read() 
+    return content
+
+
+def write_data(file_name, data):
+    f = open(file_name, "w")
+    f.write(data) 
+    f.close() 
+
+
+def prepare_lines(data):
+    splitted = data.split('\n') 
+    titles, line_1, line_2, line_3 = splitted[0], splitted[1], splitted[2], splitted[3]
+    labels = titles.split(',') 
+    line_1_formatted = line_1.split(',')
+    line_2_formatted = line_2.split(',')
+    line_3_formatted = line_3.split(',')
+    # print(line_1_formatted)
+    one = dict(zip(labels, line_1_formatted)) 
+    two = dict(zip(labels, line_2_formatted)) 
+    three = dict(zip(labels, line_3_formatted))
+    # l = [one, two, three]
+    print(one, two, three, sep="\n")
+    # print(l)
+
+    return labels, line_1_formatted, line_2_formatted, line_3_formatted, 
+def format_text(labels, line_1_formatted, line_2_formatted, line_3_formatted):
+    d = dict(zip(labels, line_1_formatted))
+    print(d)
+
+
+content = read_data("input.py")
+prepare_lines(content) 
+
