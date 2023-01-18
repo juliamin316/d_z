a43eeaeebabf462860274c89158769715defda78
juliamin316 <121152468+juliamin316@users.noreply.github.com>
2023-01-18 13:32:11 +0600

    Update task5.py

--- week_1/task5.py
+++ week_1/task5.py
@@ -1,14 +1,13 @@
-Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def find_result(strs_list):
-...     result = {}
-...     for strs in strs_list:
-...         sorted_strs = ''.join(sorted(strs))
-...         if sorted_strs in result:
-...             result[sorted_strs].append(strs)
-...         else:
-...             result[sorted_strs] = [strs]
-... 
-...     result_list = list(map(lambda x: sorted(x), list(result.values())))
-...     result_list = sorted(result_list)
-...     return result_list
+def find_result(strs_list):
+    result = {}
+    for strs in strs_list:
+        sorted_strs = ''.join(sorted(strs))
+        if sorted_strs in result:
+            result[sorted_strs].append(strs)
+        else:
+            result[sorted_strs] = [strs]
+
+    result_list = list(map(lambda x: sorted(x), list(result.values())))
+    result_list = sorted(result_list)
+    return result_list
+print(find_result(["eat","tea","tan","ate","nat","bat"]))
