d64dbaab5b745e15b7280b99a9d11bfeef2cdb2a
juliamin316 <121152468+juliamin316@users.noreply.github.com>
2023-01-24 12:07:23 +0600

    Create task2.py

---
+++ week_1/task2.py
@@ -0,0 +1,24 @@
+def read_data(file_name):
+    file = open(file_name)
+    content = file.read()
+    return content 
+    
+def write_data(file_name, data):
+    f = open(file_name, "w")
+    f.write(data)
+    f.close()
+  
+def format_text(text):
+    first_s = data.splitlines()  # подготовка для сортировки   
+    v_list = []
+    for file_names in first_s:
+        value = file_names[file_names.index('KP2.2-') + len('KP2.2-'):file_names.index(' - ')]
+        v_list.append(float(value))
+    rows_v = list(zip(v_list, first_s))
+    sorted_data = []
+    for i in sorted(rows_v):
+        sorted_data.append(str(i[1]) + '\n')
+
+    return sorted_data
+    
+write_data('output.txt', format_text(read_data('input.txt')))
