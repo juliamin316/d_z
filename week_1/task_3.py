2a8d611df42846a8cdca13b2d3f169d73cb91cd5
juliamin316 <121152468+juliamin316@users.noreply.github.com>
2023-06-19 21:58:48 +0600

    Update task_3.py

--- week_1/task_3.py
+++ week_1/task_3.py
@@ -1,26 +1,24 @@
 def read_data(file_name):
-    file = open(file_name)
-    content = file.read()
-    return content 
-    
-def write_data(file_name, data):
-    f = open(file_name, "w")
-    f.write(data)
-    f.close()
-    
-def format_t(text, symbols_q):    #symbols_q количество символов в строке 
-    lines = []
-    line = ""
-    words = text.split()
-    for word in words:
-        if len(line + " " + word) <= symbols_q:
-            line += " " + word
-        else:
-            lines.append(line.strip())
-            line = word
-    lines.append(line.strip())
-    finished_t  = "\n".join(lines)   #finished_t обработанный текст
-    return finished_t 
-  
+    with open(file_name) as file:
+        content = file.read()
+    return content
 
-write_data('output.txt', format_t(read_data('input.txt'), 21))
+def write_data(file_name, data):
+    with open(file_name, "w") as f:
+        f.write(data)
+
+def format_text(text, symbols_q):
+    lines = []
+    words = text.split()
+    current_line = words[0]
+    for word in words[1:]:
+        if len(current_line) + len(word) + 1 <= symbols_q:  # +1 для добавления пробела между словами
+            current_line += " " + word
+        else:
+            lines.append(current_line)
+            current_line = word
+    lines.append(current_line)
+    formatted_text = "\n".join(lines)
+    return formatted_text
+
+write_data('output.txt', format_text(read_data('input.txt'), 21))
