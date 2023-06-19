d02b791b282483d04d6d2deea16b2287c758b199
juliamin316 <121152468+juliamin316@users.noreply.github.com>
2023-06-19 22:04:34 +0600

    Update task_4.py

--- week_1/task_4.py
+++ week_1/task_4.py
@@ -1,18 +1,25 @@
-from collections import Counter
+def read_data(path):
+    with open(path, encoding="utf8") as file:
+        data = file.read()
+    return data
 
-# INPUT
-with open('My_text.txt', 'r') as file:
-    text = file.read()
 
-# разделим текст на слова
-words = text.split()
+def format_text(text):
+    text = text.lower().replace('.', '').replace(',', '')
+    words = text.split()
+    words_count = {}
+    for word in words:
+        words_count[word] = words_count.get(word, 0) + 1
+    sorted_words_count = sorted(words_count.items(), key=lambda x: [x[1], x[0]], reverse=True)
+    top_words_count = sorted_words_count[:10]
+    new_text = '\n'.join([f"{word},{count}" for word, count in top_words_count])
+    return new_text
 
-# создадим экземпляр Counter с помощью списка words
-word_count = Counter(words)
 
-# OUTPUT
-top_10 = word_count.most_common(10)
+def write_data(path, data):
+    with open(path, "w", encoding="utf8") as file:
+        file.write(data)
 
-# открытие файла для записи PROJECT
-with open('My_text.txt', 'a') as file:
-    for item in top_10:
+
+write_data('output.txt', format_text(read_data('input.txt')))
+
