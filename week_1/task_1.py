48f9dd374bf47d601e694777773257a740cd7469
juliamin316 <121152468+juliamin316@users.noreply.github.com>
2023-06-19 21:46:46 +0600

    Update task_1.py

--- week_1/task_1.py
+++ week_1/task_1.py
@@ -1,27 +1,28 @@
 def read_data(file_name):
-    file = open(file_name)
-    content = file.read()
+    with open(file_name) as file:
+        content = file.read()
     return content
 
 def write_data(file_name, data):
-    f = open(file_name, "w")
-    f.write(data)
-    f.close()
-    
+    with open(file_name, "w") as f:
+        f.write(data)
+
 def format_data(text):
-    students_d = {} 
-    list_f = []  
+    students_d = {}
+    list_f = []
     lines = text.split('\n')
     for line in lines:
-        line_l = line.split()  
-        name_f = line_l[1] + " " + line_l[2].strip(":") 
-        mark = line_l[3]
-        students_d.update({name_f : mark})
-        list_f.append(name_f )
-    sorted_list_f = sorted(list_f)  
+        line_l = line.split()
+        if len(line_l) >= 4:
+            name_f = line_l[1] + " " + line_l[2].strip(":")
+            mark = line_l[3]
+            students_d[name_f] = mark
+            list_f.append(name_f)
+    sorted_list_f = sorted(list_f)
     final_output = "name,grade\n"
     for name in sorted_list_f:
-        pattern = '{},{}\n'  
+        pattern = '{},{}\n'
         final_output += pattern.format(name, students_d.get(name))
-    return str(final_output)
-write_data('output.csv', format_text(read_data('data.txt')))
+    return final_output
+
+write_data('output.csv', format_data(read_data('data.txt')))
